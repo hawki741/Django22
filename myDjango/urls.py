877e155e5ed18e52939335b913f8d9197e098fe1
Kelly Lee <kmlee@duksung.ac.kr>
2022-09-28 16:38:23 +0900

    CBV로 포스트 목록/상세 페이지 만들기

--- myDjango/urls.py
+++ myDjango/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [ # IP주소
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    path('', include('single_pages.urls'))
+    path('admin/', admin.site.urls),        # IP주소/admin
+    path('blog/', include('blog.urls')),    # IP주소/blog
+    path('', include('single_pages.urls'))  # IP주소
 ]
--- single_pages/urls.py
+++ single_pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ # IP주소/
-    path('', views.landing),
-    path('about_me/', views.about_me)
+    path('', views.landing),            # IP주소
+    path('about_me/', views.about_me)   # IP주소/about_me/
 ]
