ad23431fc78bb80abcf7c5f671deb3966f7a4981
Kelly Lee <kmlee@duksung.ac.kr>
2022-10-14 13:17:24 +0900

    내비게이션바와 푸터 모듈화하기

--- blog/test.py
+++ blog/test.py
@@ -7,6 +7,20 @@ class TestView(TestCase):
     def setUp(self): # test 전에 공통적으로 실행되는 작업
         self.client = Client() # 서버구동 없이 내부적으로 실행된 client 화면 정보를 가짐
 
+    def nav_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text="InternetProgramming")
+        self.assertEqual(logo_btn.attrs['href'], '/')
+        home_btn = navbar.find('a', text="Home")
+        self.assertEqual(home_btn.attrs['href'], '/')
+        blog_btn = navbar.find('a', text="Blog")
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+        about_btn = navbar.find('a', text="About Me")
+        self.assertEqual(about_btn.attrs['href'], '/about_me/')
+
     def test_post_list(self):
         response = self.client.get('/blog/') # url : IP주소/blog/
         # response 결과가 정상적으로 왔는지
@@ -16,9 +30,7 @@ class TestView(TestCase):
         # title이 정상적으로 보이는지
         self.assertEqual(soup.title.text, 'Blog')
         # navbar가 정상적으로 보이는지
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.nav_test(soup)
 
         # post가 정상적으로 보이는지
         #1 맨 처음엔 포스트가 하나도 없음
@@ -47,9 +59,7 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)  # post_detail 수정
+        self.nav_test(soup)
 
         self.assertIn(post_001.title, soup.title.text)
 
