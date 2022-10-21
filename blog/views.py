7555789585b7850c6c104f86f3b834837b6e4015
Kelly Lee <kmlee@duksung.ac.kr>
2022-10-21 13:28:14 +0900

    미분류 category 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -27,10 +27,16 @@ class PostDetail(DetailView):
     # 매개변수는 모델명
 
 def category_page(request, slug):
-    category = Category.objects.get(slug=slug)
+    if slug == 'no_category' :
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else :
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
     return render(request, 'blog/post_list.html', {
         'category' : category,
-        'post_list' : Post.objects.filter(category=category),
+        'post_list' : post_list,
         'categories' : Category.objects.all(),
         'no_category_post_count' : Post.objects.filter(category=None).count
     })
