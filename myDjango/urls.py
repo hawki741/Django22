43b93df2d3272fcbcd38661687ea4a74789b4916
Kelly Lee <kmlee@duksung.ac.kr>
2022-11-14 17:11:18 +0900

    마크다운 적용하기

--- myDjango/urls.py
+++ myDjango/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [ # IP주소
     path('admin/', admin.site.urls),        # IP주소/admin
     path('blog/', include('blog.urls')),    # IP주소/blog
+    path('markdownx/', include('markdownx.urls')),
     path('', include('single_pages.urls'))  # IP주소
 ]
 
