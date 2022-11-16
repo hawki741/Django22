6a46e7c1d402eed6eaa34fa1486687e6606e502a
Kelly Lee <kmlee@duksung.ac.kr>
2022-11-16 16:05:55 +0900

    Comment 모델 만들기

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from markdownx.utils import markdown
 import os
 
 # Create your models here.
+
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -59,3 +60,13 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1] # .으로 분리 후 마지막
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
