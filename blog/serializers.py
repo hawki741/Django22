705914a0b8e6628487c9e3609de4f48ae8a625e2
Kelly Lee <kmlee@duksung.ac.kr>
2022-11-30 15:28:13 +0900

    User 이름 가져오기

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username',)
 
 class postSerializer(serializers.ModelSerializer):
-    # company = serializers.ReadOnlyField(source="author.username")
+    author = UserSerializer(many=False, read_only=True) 
     class Meta:
         model = Post
-        fields = ['title', 'content', 'created_at']
+        fields = ['title', 'author', 'content', 'created_at']
+
+
+
+
