172200c848761b4884af5f1b92c113e47e7e1942
Kelly Lee <kmlee@duksung.ac.kr>
2022-11-30 14:56:35 +0900

    구글차트

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Post
 
 class postSerializer(serializers.ModelSerializer):
+    # company = serializers.ReadOnlyField(source="author.username")
     class Meta:
         model = Post
         fields = ['title', 'content', 'created_at']
